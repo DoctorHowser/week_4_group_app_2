f187cee3755b00795bfc02ae596c5def5d549bea
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-11-03 15:29:20 -0600

    write routes for data

--- server/app.js
+++ server/app.js
@@ -7,16 +7,7 @@ var app = express();
 var index = require('./routes/index');
 
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/chembros');
-
-mongoose.model('Employee', new Schema({"gender": String, "salary": Number, "years": Number, "name": String,
-                                        "title": String, "rank": Number },
-                                         {collection: 'employees'}));
-
-var Employee = mongoose.model('Employee');
 
 app.set("port", process.env.PORT || 5000);
 //app.use(bodyParser.json());
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,29 @@ var router = express.Router();
 var path = require('path');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/chembros');
+
+mongoose.model('Employee', new Schema({"gender": String, "salary": Number, "years": Number, "name": String,
+        "title": String, "rank": Number },
+    {collection: 'employees'}));
+
+var Employee = mongoose.model('Employee');
+
+router.get('/data', function(req, res){
+    //handle populating ajax
+});
+
+router.post('/data', function(req, res){
+    //handle promoting ajax
+});
+
+router.delete('/data', function(req, res){
+    //handle delete ajax
+});
+
 
 router.get("/*", function(req, res){
     var file = req.params[0] || "/views/index.html";
