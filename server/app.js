01da02f14e2d2944155383ea7fabbbe88f9da054
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-11-03 15:09:30 -0600

    setup router

--- server/app.js
+++ server/app.js
@@ -4,8 +4,7 @@
 var express = require('express');
 var app = express();
 
-var path = require('path');
-var bodyParser = require('body-parser');
+var index = require('./routes/index');
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
@@ -22,10 +21,7 @@ app.set("port", process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({expanded: true}));
 
-app.get("/*", function(req, res){
-    var file = req.params[0] || "/views/index.html";
-    res.sendFile(path.join(__dirname, "./public", file));
-});
+app.get("/", index);
 
 app.listen(app.get("port"), function(){
     console.log("listening on port: ", app.get("port"));
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by danesmith on 11/3/15.
  */
+var express = require('express');
+var router = express.router();
+
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
+app.get("/*", function(req, res){
+    var file = req.params[0] || "/views/index.html";
+    res.sendFile(path.join(__dirname, "./public", file));
+});
